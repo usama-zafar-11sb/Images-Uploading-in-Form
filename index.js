b33091cdec443e5bf2a0af14888d6f99076e96ba
usama-zafar-11sb <swiftieworkers@gmail.com>
2024-09-22 19:25:41 +0500

    Images Uploading Form Using JS

---
+++ index.js
@@ -0,0 +1,17 @@
+function previewImage() {
+    const file = document.getElementById('image-input').files[0];
+    const preview = document.getElementById('preview-img');
+    const reader = new FileReader();
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+        preview.style.display = 'block';
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
+        preview.style.display = 'none';
+    }
+}
